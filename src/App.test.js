be00b6d65fbea6112325c2a7ce256338f0d7a67f
Maria Micek <mmicek16@wp.pl>
2019-04-23 14:01:35 +0200

    testing async functions - App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+const fakeData = {
+	results: [
+		{
+			name: {
+				first: 'Marysia',
+				last: 'Micek',
+			}
+		}
+	]
+}
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+const fakeFetch = () => {
+	return new Promise((resolve, reject) => {
+		resolve({
+			json: () => fakeData
+		})
+	})
+}
+
+const getRandomName = (fetch) => {
+	return fetch('https://randomuser.me/api')
+		.then(response => response.json())
+		.then(data => `${data.results[0].name.first} ${data.results[0].name.last}`)
+}
+
+describe('Using user Api', () => {
+	test('expect name to be mine', () => {
+		return expect(getRandomName(fakeFetch))
+		.resolves.toBe('Marysia Micek')
+	})
+})
